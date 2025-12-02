efd9f3bc1d39b5876d6f0b579728103645a49dc0
Cristian Redondo Cervantes <credcer2212@g.educaand.es>
2025-12-02 20:55:12 +0100

    Añado en la rama desarrollo un Scanner para poner por teclado el valor de a y b.

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("holamundo");
 		int a,b,c,d;
-		a = 7;
-		b = 8;
+		Scanner teclado = new Scanner(System.in); 
+		System.out.println("Introduzca un valor para la suma y resta(Este tiene que ser mayor que el siguiente)");
+		a = teclado.nextInt();
+		System.out.println("Introduzca un valor para la suma y resta(Este tiene que ser mayor que el siguiente)");
+		b = teclado.nextInt();
 		c = a + b;
-		d = b - a;
+		d = a - b;
 		System.out.println("La suma y la resta de " + a + " y " + b + " da como resultado: " + c + " y " + d );
+		
 	}
 
 }
